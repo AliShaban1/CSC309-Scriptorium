0bdd072b82393110cee95aa414fb5671b5fa5be4
jchionak <jchionak@icloud.com>
2024-10-31 02:57:32 -0400

    finished template post and get

--- app/pages/api/template.js
+++
@@ -1,7 +0,0 @@
-import { prisma } from "@/prisma/client"
-
-export default async function handler(req, res) {
-    if (req.method == 'POST') {
-
-    }
-}
---
+++ app/pages/api/templates/index.js
@@ -0,0 +1,150 @@
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient();
+
+const supportedLanguages = ["python", "cpp", "c", "java", "javascript"];
+const pageSize = 10;
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        let { title, code, language, explanation, tags, authorId, forked, forkedId } = req.body;
+
+        // check for absolutely necessary fields
+        if (!title || !code || !authorId || !language) {
+            return res.status(400).json({ error: "Missing required fields." });
+        }
+
+        // add default values to fields that aren't required
+        if (!explanation) {
+            explanation = "";
+        }
+
+        if (!forked) {
+            forked = false;
+        }
+
+        // convert to proper data types
+        forked = (forked === 'true');
+        authorId = parseInt(authorId);
+        if (forkedId) {
+            forkedId = parseInt(forkedId);
+        }
+
+        // ensure author is valid
+        // this is where we should also authenticate the user
+        let author = await prisma.user.findUnique({
+            where: {
+                id: authorId
+            }
+        });
+        if (author === null) {
+            return res.status(400).json({ error: "Invalid user." });
+        }
+
+        // if it has been forked from another template, make sure it exists
+        if (forked) {
+            let forkedTemplate = await prisma.template.findFirst({
+                where: {
+                    id: forkedId
+                }
+            });
+            if (forkedTemplate === null) {
+                // if the forkedId doesn't exist, just say it's not forked
+                forked = false;
+            };
+        }
+
+        // make sure language is actually supported
+        if (!supportedLanguages.includes(language)) {
+            return res.status(400).json({ error: "Unsupported language." });
+        }
+
+        // if there are tags, we need to split them by comma
+        let tagConnections = [];
+        if (tags) {
+            const tagArray = tags.split(',').map(tag => tag.trim().toLowerCase());
+            tagConnections = tagArray.map(tag => ({
+                where: { name: tag },
+                create: { name: tag }
+            }));
+        }
+
+        // now, save the actual template
+        let template;
+        if (forked) {
+            template = await prisma.template.create({
+                data: {
+                    title,
+                    code,
+                    language,
+                    explanation,
+                    tags: {
+                        connectOrCreate: tagConnections
+                    },
+                    author: { connect: { id: authorId } },
+                    forked,
+                    forkedFrom: { connect: { id: forkedId } }
+                }
+            });
+        } else {
+            template = await prisma.template.create({
+                data: {
+                    title,
+                    code,
+                    language,
+                    explanation,
+                    tags: {
+                        connectOrCreate: tagConnections
+                    },
+                    author: { connect: { id: authorId } },
+                    forked
+                }
+            });
+        }
+
+        return res.status(201).json(template);
+
+    } else if (req.method === 'GET') {
+        const { id, title, code, tags, authorId, page } = req.body;
+        const filters = {};
+
+        // add the search filters if they are actually valid
+        if (id && Number(id)) {
+            filters.id = Number(id);
+        }
+        if (authorId && Number(authorId)) {
+            filters.authorId = Number(authorId);
+        }
+        if (title) {
+            filters.title = {
+                contains: title,
+            }
+        }
+        if (code) {
+            filters.code = {
+                contains: code,
+            }
+        }
+        if (tags) {
+            let tagArray = tags.split(',').map(tag => tag.trim().toLowerCase());
+            filters.tags = {
+                some: {
+                    name: { in: tagArray }
+                }
+            }
+        }
+        let pageNumber = 1;
+        if (page && Number(page) && Number(page) > 0) {
+            pageNumber = parseInt(page);
+        }
+
+        const templates = await prisma.template.findMany({ where: filters, include: { author: true, tags: true } });
+        const firstOnPage = (pageNumber - 1) * pageSize;
+
+        const templatesPage = templates.slice(firstOnPage, firstOnPage + pageSize - 1);
+
+        return res.status(201).json({ results: templatesPage });
+    } else {
+        return res.status(400).json({ error: "Method not supported." })
+    }
+}
