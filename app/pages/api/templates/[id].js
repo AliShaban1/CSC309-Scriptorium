687229bca27f494ed5c7c69aa39390b112620445
jchionak <jchionak@icloud.com>
2024-11-03 16:01:31 -0500

    template routes fixed

--- app/pages/api/templates/[id].js
+++ app/pages/api/templates/[id].js
@@ -1,94 +1,97 @@
+import { protect } from '../../../middleware/auth';
+
 const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient();
 
-export default async function handler(req, res) {
-    if (req.method === "PUT") {
-        const token = req.headers.authorization.split(' ')[1];
-        const { id } = req.query;
-        const { title, explanation, tags, code } = req.body;
+const updateTemplate = async (req, res) => {
+    const { id } = req.query;
+    const { title, explanation, tags, code } = req.body;
 
-        if (!token) {
-            return res.status(401).json({ message: 'Authentication required' });
-        }
+    const userId = Number(req.userId);
 
-        const { userId } = jwt.verify(token, process.env.JWT_SECRET);
+    // check that id is actually valid
+    if (!Number(id)) {
+        return res.status(404).json({ error: "Invalid ID." })
+    }
 
-        // check that id is actually valid
-        if (!Number(id)) {
-            return res.status(404).json({ error: "Invalid ID." })
-        }
+    const initialTemplate = await prisma.template.findUnique({
+        where: { id: Number(id) },
+        include: { author: true }
+    });
 
-        const initialTemplate = prisma.template.findUnique({
-            where: { id: Number(id), include: { author: true } }
+    if (!initialTemplate) {
+        return res.status(404).json({ error: "Invalid ID." });
+    } else if (initialTemplate.author.id !== userId) {
+        return res.status(401).json({ error: "Cannot update a template that doesn't belong to you!" })
+    }
+
+    const data = {};
+    if (title) {
+        data.title = title;
+    }
+
+    if (explanation) {
+        data.explanation = explanation;
+    }
+
+    if (tags) {
+        const tagArray = tags.split(',').map(tag => tag.trim().toLowerCase());
+        const tagConnections = tagArray.map(tag => ({
+            where: { name: tag },
+            create: { name: tag }
+        }));
+        data.tags = { connectOrCreate: tagConnections };
+    }
+
+    if (code) {
+        data.code = code;
+    }
+
+    try {
+        const template = await prisma.template.update({
+            where: { id: Number(id) },
+            data: data,
+        });
+
+        return res.status(200).json(template);
+    } catch (error) {
+        return res.status(404).json({ error: "Invalid ID or data." })
+    }
+}
+
+const deleteTemplate = async (req, res) => {
+    const { id } = req.query;
+    if (!Number(id)) {
+        return res.status(404).json({ error: "Invalid ID." });
+    }
+
+    const userId = Number(req.userId);
+    try {
+        const initialTemplate = await prisma.template.findUnique({
+            where: { id: Number(id) },
+            include: { author: true }
         });
 
         if (!initialTemplate) {
             return res.status(404).json({ error: "Invalid ID." });
         } else if (initialTemplate.author.id !== userId) {
-            return res.status(401).json({ error: "Cannot update a template that does not belong to you!" })
+            return res.status(401).json({ error: "Cannot delete a template that does not belong to you!" })
         }
+        const deleted = await prisma.template.delete({
+            where: { id: Number(id) },
+        })
+        return res.status(200).json(deleted);
+    } catch (error) {
+        return res.status(404).json({ error: `Error: ${error}` })
+    }
+}
 
-        const data = {};
-        if (title) {
-            data.title = title;
-        }
-
-        if (explanation) {
-            data.explanation = explanation;
-        }
-
-        if (tags) {
-            const tagArray = tags.split(',').map(tag => tag.trim().toLowerCase());
-            const tagConnections = tagArray.map(tag => ({
-                where: { name: tag },
-                create: { name: tag }
-            }));
-            data.tags = { connectOrCreate: tagConnections };
-        }
-
-        if (code) {
-            data.code = code;
-        }
-
-        try {
-            const template = await prisma.template.update({
-                where: { id: Number(id) },
-                data: data,
-            });
-
-            return res.status(200).json(template);
-        } catch (error) {
-            return res.status(404).json({ error: "Invalid ID or data." })
-        }
+export default async function handler(req, res) {
+    if (req.method === "PUT") {
+        return protect(updateTemplate)(req, res);
     } else if (req.method === "DELETE") {
-        const token = req.headers.authorization.split(' ')[1];
-        const { id } = req.query;
-        if (!Number(id)) {
-            return res.status(404).json({ error: "Invalid ID." });
-        }
-        if (!token) {
-            return res.status(401).json({ message: 'Authentication required' });
-        }
-
-        const { userId } = jwt.verify(token, process.env.JWT_SECRET);
-        try {
-            const initialTemplate = prisma.template.findUnique({
-                where: { id: Number(id), include: { author: true } }
-            });
-
-            if (!initialTemplate) {
-                return res.status(404).json({ error: "Invalid ID." });
-            } else if (initialTemplate.author.id !== userId) {
-                return res.status(401).json({ error: "Cannot delete a template that does not belong to you!" })
-            }
-            const deleted = await prisma.template.delete({
-                where: { id: Number(id) },
-            })
-            return res.status(200).json(deleted);
-        } catch (error) {
-            return res.status(404).json({ error: "Invalid ID." })
-        }
+        return protect(deleteTemplate)(req, res);
     } else if (req.method === "GET") {
         const { id } = req.query;
         if (!Number(id)) {
@@ -97,7 +100,7 @@ export default async function handler(req, res) {
         try {
             const requested = await prisma.template.findFirst({
                 where: { id: Number(id) },
-                include: { author: true, tags: true, blogPosts: true }
+                include: { tags: true, blogPosts: true }
             })
             if (requested === null) {
                 return res.status(404).json({ error: `Template with ID ${id} not found.` })
--- app/pages/api/templates/index.js
+++ app/pages/api/templates/index.js
@@ -1,122 +1,118 @@
 const { PrismaClient } = require('@prisma/client')
-import jwt from 'jsonwebtoken';
+import { protect } from '../../../middleware/auth';
 
 const prisma = new PrismaClient();
 
 const supportedLanguages = ["python", "cpp", "c", "java", "javascript"];
 const pageSize = 10;
 
+const createTemplate = async (req, res) => {
+    let { title, code, language, explanation, tags, forked, forkedId } = req.body;
+
+    const userId = req.userId;
+    let authorId = Number(userId);
+
+    // check for absolutely necessary fields
+    if (!title || !code || !authorId || !language) {
+        return res.status(400).json({ error: "Missing required fields." });
+    }
+
+    // add default values to fields that aren't required
+    if (!explanation) {
+        explanation = "";
+    }
+
+    if (!forked) {
+        forked = false;
+    }
+
+    // convert to proper data types
+    forked = (forked === 'true');
+
+
+    if (forkedId) {
+        forkedId = parseInt(forkedId);
+    }
+
+    // ensure author is valid
+    // this is where we should also authenticate the user
+    let author = await prisma.user.findUnique({
+        where: {
+            id: authorId
+        }
+    });
+    if (author === null) {
+        return res.status(400).json({ error: "Invalid user." });
+    }
+
+    // if it has been forked from another template, make sure it exists
+    if (forked) {
+        let forkedTemplate = await prisma.template.findFirst({
+            where: {
+                id: forkedId
+            }
+        });
+        if (forkedTemplate === null) {
+            // if the forkedId doesn't exist, just say it's not forked
+            forked = false;
+        };
+    }
+
+    // make sure language is actually supported
+    if (!supportedLanguages.includes(language)) {
+        return res.status(400).json({ error: "Unsupported language." });
+    }
+
+    // if there are tags, we need to split them by comma
+    let tagConnections = [];
+    console.log(tags);
+    if (tags) {
+        const tagArray = tags.split(',').map(tag => tag.trim().toLowerCase());
+        tagConnections = tagArray.map(tag => ({
+            where: { name: tag },
+            create: { name: tag }
+        }));
+    }
+
+    // now, save the actual template
+    let template;
+    if (forked) {
+        template = await prisma.template.create({
+            data: {
+                title,
+                code,
+                language,
+                explanation,
+                tags: {
+                    connectOrCreate: tagConnections
+                },
+                author: { connect: { id: authorId } },
+                forked,
+                forkedFrom: { connect: { id: forkedId } }
+            }
+        });
+    } else {
+        template = await prisma.template.create({
+            data: {
+                title,
+                code,
+                language,
+                explanation,
+                tags: {
+                    connectOrCreate: tagConnections
+                },
+                author: { connect: { id: authorId } },
+                forked
+            }
+        });
+    }
+
+    return res.status(201).json(template);
+}
+
 export default async function handler(req, res) {
     if (req.method === 'POST') {
-        try {
-            const token = req.headers.authorization.split(' ')[1];
-            let { title, code, language, explanation, tags, forked, forkedId } = req.body;
-
-            if (!token) {
-                return res.status(401).json({ message: 'Authentication required' });
-            }
-
-            const { userId } = jwt.verify(token, process.env.JWT_SECRET);
-
-            // check for absolutely necessary fields
-            if (!title || !code || !authorId || !language) {
-                return res.status(400).json({ error: "Missing required fields." });
-            }
-
-            // add default values to fields that aren't required
-            if (!explanation) {
-                explanation = "";
-            }
-
-            if (!forked) {
-                forked = false;
-            }
-
-            // convert to proper data types
-            forked = (forked === 'true');
-
-            let authorId = userId;
-
-            if (forkedId) {
-                forkedId = parseInt(forkedId);
-            }
-
-            // ensure author is valid
-            // this is where we should also authenticate the user
-            let author = await prisma.user.findUnique({
-                where: {
-                    id: authorId
-                }
-            });
-            if (author === null) {
-                return res.status(400).json({ error: "Invalid user." });
-            }
-
-            // if it has been forked from another template, make sure it exists
-            if (forked) {
-                let forkedTemplate = await prisma.template.findFirst({
-                    where: {
-                        id: forkedId
-                    }
-                });
-                if (forkedTemplate === null) {
-                    // if the forkedId doesn't exist, just say it's not forked
-                    forked = false;
-                };
-            }
-
-            // make sure language is actually supported
-            if (!supportedLanguages.includes(language)) {
-                return res.status(400).json({ error: "Unsupported language." });
-            }
-
-            // if there are tags, we need to split them by comma
-            let tagConnections = [];
-            if (tags) {
-                const tagArray = tags.split(',').map(tag => tag.trim().toLowerCase());
-                tagConnections = tagArray.map(tag => ({
-                    where: { name: tag },
-                    create: { name: tag }
-                }));
-            }
-
-            // now, save the actual template
-            let template;
-            if (forked) {
-                template = await prisma.template.create({
-                    data: {
-                        title,
-                        code,
-                        language,
-                        explanation,
-                        tags: {
-                            connectOrCreate: tagConnections
-                        },
-                        author: { connect: { id: authorId } },
-                        forked,
-                        forkedFrom: { connect: { id: forkedId } }
-                    }
-                });
-            } else {
-                template = await prisma.template.create({
-                    data: {
-                        title,
-                        code,
-                        language,
-                        explanation,
-                        tags: {
-                            connectOrCreate: tagConnections
-                        },
-                        author: { connect: { id: authorId } },
-                        forked
-                    }
-                });
-            }
-
-            return res.status(201).json(template);
-        } catch (error) {
-            return res.status(400).json({ error: "Unable to upload template." });
-        }
+        return protect(createTemplate)(req, res);
 
     } else if (req.method === 'GET') {
         const { id, title, code, tags, authorId, page } = req.body;
@@ -152,7 +148,7 @@ export default async function handler(req, res) {
             pageNumber = parseInt(page);
         }
 
-        const templates = await prisma.template.findMany({ where: filters, include: { author: true, tags: true } });
+        const templates = await prisma.template.findMany({ where: filters, include: { tags: true } });
         const firstOnPage = (pageNumber - 1) * pageSize;
 
         const templatesPage = templates.slice(firstOnPage, firstOnPage + pageSize - 1);
